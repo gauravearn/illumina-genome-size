f5aa369e1d0ff189127ab077ecfd011791fa27d2
Gaurav Sablok <sablokg@gmail.com>
2023-10-23 14:35:23 +0530

    Update r_function_to_stack.R

--- r_function_to_stack.R
+++ r_function_to_stack.R
@@ -1,9 +1,13 @@
+#!/usr/bin/R
+# -*- coding:  utf-8 -*-
+# Author: Gaurav Sablok
+# date: 2023-10-23
 stacking_option <- function(){
     library(dplyr)
     library(magick)
-image_path <- character(length(list.files(path=".", pattern="*.jpg")))
+image_path <- character(length(list.files(path=".", pattern="*.png")))
 for (i in seq_along(list(path=".", pattern="*.png"))){
-    image_path[i] <- paste0(getwd(),list.files(path=".", pattern="*.jpg")[i])
+    image_path[i] <- paste0(getwd(),list.files(path=".", pattern="*.png")[i])
     a <- image_path[1]
     b <- image_path[2]
     c <- image_path[3]
